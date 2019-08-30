b65f462004d1382212d67a5d31cd3a80c73b50aa
Jakob Palmer <jakempalmer@gmail.com>
2019-08-29 22:00:05 -0700

    First Commit

---
+++ Assets/Scripts/SetupScript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.Random;
+
+public class SetupScript : MonoBehaviour
+{
+
+	//Random random = new Random();
+	//UnityEngine.Random random = new UnityEngine.Random();
+
+	protected int WORLD_SIZE = 20;
+
+	public GameObject dirtBlock;
+	public GameObject grassBlock;
+	public GameObject airBlock;
+	private GameObject current_ground;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		InstantiateMap(WORLD_SIZE);
+
+	}
+
+	public void InstantiateMap(int WorldSize)
+	{
+			
+			int count = 0;
+
+			for (int i = 0; i < WorldSize; i++) {			
+				for (int j = 0; j < WorldSize; j++) {
+
+					float rNum = (Random.value * 100);
+					Debug.Log(rNum);
+
+					if(rNum <= 40 )
+					{
+						current_ground = dirtBlock;
+					} else if (rNum < 80 && rNum > 40 ){
+						current_ground = grassBlock;
+					} else {	
+						current_ground = airBlock;
+					}
+
+					Instantiate(current_ground, new Vector3(i, 0, j), Quaternion.identity);
+					count++;
+
+				} // FOR (j)	
+			} // FOR (i)
+
+			Debug.Log(count);
+	}
+
+	public void SpawnBunnies(int count)
+	{
+
+
+	}
+
+
+
+
+} // CLASS
