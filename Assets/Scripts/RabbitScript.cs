d9c7a1857d66f6c953b02203028aa9e9216262fe
Jakob <jakobmpalmer@gmail.com>
2019-09-04 22:02:28 -0700

    Added Rabbits and Turnips

---
+++ Assets/Scripts/RabbitScript.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RabbitScript : MonoBehaviour
+{
+
+    int health;
+    int hunger;
+    int energy;
+
+public GameObject rabbitHealthbar;
+int height = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 100;
+        hunger = 100;
+        energy = 100;
+
+        Instantiate(rabbitHealthbar,this.transform.position + Vector3.up * height, Quaternion.identity);
+         Debug.DrawLine(Vector3.zero, new Vector3(5, 0, 0), Color.red, 100.5f);
+        //StartCoroutine(Move());
+        StartCoroutine(WaitAndMove(3));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        WaitAndMove(3);
+    }
+
+ IEnumerator Move()
+    {
+        int moveDistance = 1;
+
+        print(Time.time);
+        yield return new WaitForSeconds(3);
+        this.transform.Translate(new Vector3(1, 0, 0));
+        print(Time.time);
+
+        // this.transform.Translate(new Vector3.Lerp(this.transform.position, this.transform.position + Vector3.right * moveDistance, 1.0f));
+        // print(Time.time);
+        
+        // transform.position=Vector3.Lerp(this.transform.position, this.transform.position + Vector3.right* moveDistance,Time.time-startTime);
+
+        
+    }
+
+    IEnumerator WaitAndMove(float delayTime){
+     yield return new WaitForSeconds(delayTime); // start at time X
+     float startTime=Time.time; // Time.time contains current frame time, so remember starting point
+     //while(Time.time-startTime<=1){ // until one second passed
+         //transform.position=Vector3.Lerp(this.transform.position, this.transform.position + Vector3.right,Time.time-startTime); // lerp from A to B in one second
+         this.transform.Translate(new Vector3(1, 0, 0));
+         yield return 1; // wait for next frame
+     //}
+    }
+    
+
+
+    void SearchForFood()
+    {
+//Rotate random direction for second
+
+//if food isnt in front of fov, turn again, if not remain on course
+
+    }
+
+    void Sleep()
+    {
+
+
+    }
+
+    void Eat()
+    {
+
+
+    }
+
+
+    void Mate()
+    {
+
+        
+    }
+
+} // Class
--- Assets/Scripts/SetupScript.cs
+++ Assets/Scripts/SetupScript.cs
@@ -9,6 +9,7 @@ public class SetupScript : MonoBehaviour
 	//Random random = new Random();
 	//UnityEngine.Random random = new UnityEngine.Random();
 
+//World Spawn
 	protected int WORLD_SIZE = 20;
 
 	public GameObject dirtBlock;
@@ -16,17 +17,30 @@ public class SetupScript : MonoBehaviour
 	public GameObject airBlock;
 	private GameObject current_ground;
 
+	public GameObject barrier;
+
+//Fauna Spawn
+	public GameObject rabbitPrefab;
+
+	public GameObject turnipPrefab;
+
+	private GameObject[] theMap;
+
     // Start is called before the first frame update
     void Start()
     {
+		theMap = new GameObject[WORLD_SIZE * WORLD_SIZE];
 		InstantiateMap(WORLD_SIZE);
-
+		CreateBarrier();
+		SpawnFauna(5);
+		SpawnFlora();
 	}
 
 	public void InstantiateMap(int WorldSize)
 	{
 			
 			int count = 0;
+			
 
 			for (int i = 0; i < WorldSize; i++) {			
 				for (int j = 0; j < WorldSize; j++) {
@@ -43,7 +57,8 @@ public class SetupScript : MonoBehaviour
 						current_ground = airBlock;
 					}
 
-					Instantiate(current_ground, new Vector3(i, 0, j), Quaternion.identity);
+					theMap[count] = Instantiate(current_ground, new Vector3(i, 0, j), Quaternion.identity);
+					
 					count++;
 
 				} // FOR (j)	
@@ -52,11 +67,58 @@ public class SetupScript : MonoBehaviour
 			Debug.Log(count);
 	}
 
-	public void SpawnBunnies(int count)
+	public void SpawnFauna(int count)
 	{
+		for(int i = 0; i < count; i++){
+			float rNumX = (Random.value * WORLD_SIZE);
+			float rNumY = (Random.value * WORLD_SIZE);
 
-
+			Vector3 randomLoc = new Vector3(rNumX, 1, rNumY);
+			Instantiate(rabbitPrefab, randomLoc, Quaternion.identity);
+		}
 	}
+
+	public void SpawnFlora()
+	{
+		float rNum;
+		// for each block
+		int count = 0;
+		for(int i = 0; i < WORLD_SIZE-1; i++){
+			for(int j = 0; j < WORLD_SIZE; j++){
+				Debug.Log("count:: " + count);
+				Debug.Log("BLOCK: " + theMap[count]);
+				rNum = (Random.value);
+			// if it is a grass block
+			if((theMap[count].name == "GrassBlock(Clone)") && (rNum > 0.40f)){
+				// then spawn 3-6 plants on block (in random locations)
+				//Debug.Log("Plant here!! @ i: " + i + " | j: " + j + " || COUNT:: " + count);
+				if((rNum * 100) % 3 == 1)
+				{
+					//Instantiate(turnipPrefab, new Vector3(theMap[count].transform.position.x + rNum, 0.61f, theMap[count].transform.position.z), Quaternion.identity);
+					//Instantiate(turnipPrefab, new Vector3(theMap[count].transform.position.x, 0.61f, theMap[count].transform.position.z), Quaternion.identity);
+				}
+				//Instantiate(turnipPrefab, new Vector3(theMap[count].transform.position.x - rNum, 0.61f, theMap[count].transform.position.z), Quaternion.identity);
+				Instantiate(turnipPrefab, new Vector3(theMap[count].transform.position.x, 0.61f, theMap[count].transform.position.z), Quaternion.identity);
+				}
+				count++;
+			}
+		}
+	}
+
+	public void CreateBarrier()
+	{
+		int WorldBound = WORLD_SIZE++;
+
+		Instantiate(barrier, new Vector3(-1, 0, -1), Quaternion.identity);
+
+		for(int i = 0; i < WORLD_SIZE; i++)
+		{
+			Instantiate(barrier, new Vector3(i, 0, -1), Quaternion.identity);
+			Instantiate(barrier, new Vector3(-1, 0, i), Quaternion.identity);
+			Instantiate(barrier, new Vector3(WorldBound, 0, i), Quaternion.identity);
+			Instantiate(barrier, new Vector3(i, 0, WorldBound), Quaternion.identity);
+		}
+	} // create barrier
 
 
 
